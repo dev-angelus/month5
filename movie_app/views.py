747f61ce0cb3d02f5c7362f2597aa53e80b6f945
maxcrimea <maxcrimea50@gmail.com>
2023-05-09 23:07:56 +0600

    hw_1

---
+++ movie_app/views.py
@@ -0,0 +1,60 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .models import Director, Movie, Review
+from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
+
+
+@api_view(['GET'])
+def director_list_api_view(request):
+    director = Director.objects.all()
+    serializer = DirectorSerializer(director, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def director_detail_api_view(request, id):
+    try:
+        director = Director.objects.get(id=id)
+    except Director.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={'error': 'Object not found!'})
+    serializer = DirectorSerializer(director)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def movie_list_api_view(request):
+    movie = Movie.objects.all()
+    serializer = MovieSerializer(movie, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def movie_detail_api_view(request, id):
+    try:
+        movie = Movie.objects.get(id=id)
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={'error': 'Object not found!'})
+    serializer = MovieSerializer(movie)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def review_list_api_view(request):
+    review = Review.objects.all()
+    serializer = ReviewSerializer(review, many=True)
+    return Response(data=serializer.data)
+
+
+@api_view(['GET'])
+def review_detail_api_view(request, id):
+    try:
+        review = Review.objects.get(id=id)
+    except Review.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={'error': 'Object not found!'})
+    serializer = ReviewSerializer(review)
+    return Response(data=serializer.data)
